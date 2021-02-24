2f859cb0d62adb53e832e4755b53e3104528bf7b
奚渭雄 <xiweixiong@iwubida.com>
2021-02-24 09:55:11 +0800

    优化内容

--- src/cmd.ts
+++ src/cmd.ts
@@ -3,11 +3,23 @@ import * as path from 'path'
 import * as inquirer from 'inquirer'
 import * as fs from 'fs-extra'
 import * as chalk from 'chalk'
+import * as prettier from 'prettier'
 import { upperCase, getName } from './utils'
 
 const packageFilePath = path.join(__dirname, '..', 'package.json')
 const packageInfo = JSON.parse(fs.readFileSync(packageFilePath, 'utf8'))
 const currentVersion = packageInfo.version
+const prettierConfig = {
+  parser: 'babel',
+  printWidth: 200,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+}
 
 program.version(currentVersion).usage('[命令] [配置项]')
 ;(async function () {
@@ -58,7 +70,9 @@ program.version(currentVersion).usage('[命令] [配置项]')
             if (!fs.existsSync(moduleDir)) {
               fs.mkdirSync(moduleDir)
               console.log(chalk.green(`创建模块文件夹：${chalk.yellow(moduleDir)}`))
+            }
 
+            if (!fs.existsSync(moduleMobxPath)) {
               // 创建模块mobx
               const moduleMobxTemp = fs.readFileSync(path.join(__dirname, '../template/module-mobx.tp')).toString()
               fs.writeFileSync(moduleMobxPath, moduleMobxTemp.replace(/\$moduleName\$/g, moduleName))
@@ -68,8 +82,9 @@ program.version(currentVersion).usage('[命令] [配置项]')
               let mobx = fs.readFileSync(mobxPath).toString()
               mobx = `import ${moduleName} from '@/views/${newModuleName}/mobx'\n${mobx}`
               let exstr = `/** ${moduleName} 模块 mobx */\n  ...${moduleName},`
-              const mathcs = mobx.match(/const.*store.*=.*{/)
-              if (mathcs.length > 0) mobx = mobx.replace(mathcs[0], `${mathcs[0]}\n  ${exstr}`)
+              const mathcs = mobx.match(/const.*store.*=.*{/g)
+              if (mathcs.length > 0) mobx = mobx.replace(mathcs[0], `${mathcs[0]}\n${exstr}`)
+              mobx = prettier.format(mobx, prettierConfig)
               fs.writeFileSync(mobxPath, mobx)
               console.log(chalk.green(`注入mobx：${chalk.yellow(mobxPath)}`))
             }
@@ -77,9 +92,11 @@ program.version(currentVersion).usage('[命令] [配置项]')
             if (!fs.existsSync(pkgDir)) {
               fs.mkdirSync(pkgDir)
               console.log(chalk.green(`创建package文件夹：${chalk.yellow(pkgDir)}`))
+            }
 
-              // 在包下面创建mobx
-              const pkgMobxPath = `${pkgDir}/mobx.ts`
+            // 在包下面创建mobx
+            const pkgMobxPath = `${pkgDir}/mobx.ts`
+            if (!fs.existsSync(pkgMobxPath)) {
               const pkgMobxTemp = fs.readFileSync(path.join(__dirname, '../template/pkg-mobx.tp')).toString()
               fs.writeFileSync(pkgMobxPath, pkgMobxTemp.replace(/\$pkgName\$/g, upperCase(pkgMobxName)))
               console.log(chalk.green(`创建package mobx：${chalk.yellow(pkgMobxPath)}`))
@@ -88,8 +105,9 @@ program.version(currentVersion).usage('[命令] [配置项]')
               let moduleMobx = fs.readFileSync(moduleMobxPath).toString()
               moduleMobx = `import ${pkgMobxName} from './${packageName}/mobx'\n${moduleMobx}`
               let exstr = `/** ${pkgMobxName} mobx */\n  ${pkgMobxName},`
-              const matchs = moduleMobx.match(/const.*Store.*=.*{/)
+              const matchs = moduleMobx.match(/const.*Store.*=.*{/g)
               if (matchs.length > 0) moduleMobx = moduleMobx.replace(matchs[0], `${matchs[0]}\n ${exstr}`)
+              moduleMobx = prettier.format(moduleMobx, prettierConfig)
               fs.writeFileSync(moduleMobxPath, moduleMobx)
               console.log(chalk.green(`注入模块mobx：${chalk.yellow(moduleMobxPath)}`))
             }
@@ -109,7 +127,7 @@ program.version(currentVersion).usage('[命令] [配置项]')
             console.log(chalk.green(`生成视图：${chalk.yellow(viewPath)}`))
             // 创建style
             const stylePath = `${viewDir}/style.ts`
-            let styleStr = fs.readFileSync(path.join(__dirname, '../template/style-view.tp')).toString()
+            let styleStr = fs.readFileSync(path.join(__dirname, `../template/style-${viewType}.tp`)).toString()
             fs.writeFileSync(stylePath, styleStr)
             console.log(chalk.green(`生成style：${chalk.yellow(stylePath)}`))
 
@@ -123,25 +141,53 @@ program.version(currentVersion).usage('[命令] [配置项]')
 
                 const mainRouterPath = './src/router/index.tsx'
                 let mainRouterStr = fs.readFileSync(mainRouterPath).toString()
+                // 添加引用
                 const constStr = `const ${upperCase(moduleName)}Router = loadable(() => import('./${newModuleName}'))`
-                const constReplaceStr = `${constStr}\n/**constviews*/`
+                const constMatchs = mainRouterStr.match(/const.*Router.*=.*loadable\(\(\).*=>.*import\(.*\)\)/g)
+                if (constMatchs) {
+                  const tag = constMatchs[constMatchs.length - 1]
+                  mainRouterStr = mainRouterStr.replace(tag, `${tag}\n${constStr}`)
+                } else {
+                  const importMatchs = mainRouterStr.match(/import.*from.*/g)
+                  const tag = importMatchs[importMatchs.length - 1]
+                  mainRouterStr = mainRouterStr.replace(tag, `${tag}\n\n// 模块路由\n${constStr}`)
+                }
+                // 添加路由标签
                 const routerNodeStr = `<Route path="/${moduleName}" component={${upperCase(moduleName)}Router} />`
-                const routerNodeReplaceStr = `${routerNodeStr}\n          {/**views*/}`
-                mainRouterStr = mainRouterStr.replace(/\/\*\*constviews\*\//g, constReplaceStr)
-                mainRouterStr = mainRouterStr.replace(/\{\/\*\*views\*\/\}/g, routerNodeReplaceStr)
+                const routerMatchs = mainRouterStr.match(/<\/Switch>/g)
+                if (routerMatchs.length <= 0) {
+                  throw new Error('主路由没有找到Switch标签')
+                }
+                mainRouterStr = mainRouterStr.replace(routerMatchs[0], `${routerNodeStr}\n</Switch>`)
+                mainRouterStr = prettier.format(mainRouterStr, prettierConfig)
+                // 写入文件
                 fs.writeFileSync(mainRouterPath, mainRouterStr)
                 console.log(chalk.green(`生成模块路由并注入主路由`))
               }
 
               // 注入模块路由
               let routerStr = fs.readFileSync(routerPath).toString()
+              // 添加引用
               const constStr = `const ${pkgViewName}View = loadable(() => import('@/views/${newModuleName}/${packageName}/${viewName}'))`
-              const constReplaceStr = `${constStr}\n/**constviews*/`
+              const constMatchs = routerStr.match(/const.*View.*=.*loadable\(\(\).*=>.*import\(.*\)\)/g)
+              if (constMatchs) {
+                const tag = constMatchs[constMatchs.length - 1]
+                routerStr = routerStr.replace(tag, `${tag}\n${constStr}`)
+              } else {
+                const importMatchs = routerStr.match(/import.*from.*/g)
+                const tag = importMatchs[importMatchs.length - 1]
+                routerStr = routerStr.replace(tag, `${tag}\n\n// 页面路由\n${constStr}`)
+              }
+              // 添加路由标签
               const viewRouterPath = '`${path}/' + packageName + '/' + viewName + '`'
               const viewNodeStr = `<Route path={${viewRouterPath}}><KeepAlive name={${viewRouterPath}}><${pkgViewName}View /></KeepAlive></Route>`
-              const viewNodeReplaceStr = `${viewNodeStr}\n      {/**views*/}`
-              routerStr = routerStr.replace(/\/\*\*constviews\*\//g, constReplaceStr)
-              routerStr = routerStr.replace(/\{\/\*\*views\*\/\}/g, viewNodeReplaceStr)
+              const routerMatchs = routerStr.match(/<\/Switch>/g)
+              if (routerMatchs.length <= 0) {
+                throw new Error('模块路由没有找到Switch标签')
+              }
+              routerStr = routerStr.replace(routerMatchs[0], `${viewNodeStr}\n</Switch>`)
+              routerStr = prettier.format(routerStr, prettierConfig)
+              // 写入文件
               fs.writeFileSync(routerPath, routerStr)
               console.log(chalk.green(`注入模块路由`))
             }
